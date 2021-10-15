d33cfe9b27e7f47adcb0a52efef320665eb89502
Naim Shihab <78292955+naimshihab@users.noreply.github.com>
2021-10-15 16:05:07 +0800

    Add files via upload

---
+++ latihanCallback.js
@@ -0,0 +1,68 @@
+$('.search-button').on('click', function() {
+
+  $.ajax({
+    url: 'http://www.omdbapi.com/?apikey=5491c717&s=' + $('.input-keyword1').val(),
+    success: results => {
+      const movies = results.Search;
+      let card = '';
+      movies.forEach(m => {
+        card += showCards(m);
+      });
+      $('.movie-container').html(card);
+  
+      // Ketika tombol detail di klik
+      $('.modal-detail-button').on('click', function() {
+        $.ajax({
+          url: 'http://www.omdbapi.com/?apikey=5491c717&i=' + $(this).data('imdbid'),
+          success: m => {
+            const movieDetail = showMovieDetail(m);
+            $('.modal-body').html(movieDetail);
+          },
+          error: (e) => {
+            console.log(e.responseTect);
+          }
+        });
+      });
+  
+    }, 
+    error: (e) => {
+      console.log(e.responseTect);
+    }
+  });
+});
+
+
+function showCards(m) {
+  return `<div class="col-md-4 my-3">
+          <div class="card">
+            <img src="${m.Poster}" class="card-img-top">
+            <div class="card-body">
+              <h5 class="card-title">${m.Title}</h5>
+              <h6 class="card-subtitle mb-2 text-muted">${m.Year}</h6>
+              <a href="#" class="btn btn-primary modal-detail-button" data-toggle="modal" data-target="#movieDetalModal" data-imdbid="${m.imdbID}">Show Details</a>
+            </div>
+          </div>
+          </div>`;
+}
+
+function showMovieDetail(m) {
+  return `<div class="container-fluid">
+          <div class="row">
+            <div class="col-md-3">
+              <img src="${m.Poster}" class="img-fluid">
+            </div>
+
+            <div class="col-md">
+              <ul class="list-group">
+                <li class="list-group-item"><h4>${m.Title} (${m.Year})</h4></li>
+                <li class="list-group-item"><strong>Director : </strong> ${m.Director}</li>
+                <li class="list-group-item"><strong>Actors : </strong> ${m.Actors}</li>
+                <li class="list-group-item"><strong>Writer : </strong> ${m.Writer}</li>
+                <li class="list-group-item"><strong>Plot : </strong> <br> ${m.Plot}</li>
+              </ul>
+            </div>
+
+          </div>
+          </div>`;
+}
+
